9d37d41d7eaea8ce1ea1c4caf5be0ddf884cb361
KarlDoreth <30433846+KarlDoreth@users.noreply.github.com>
2020-03-04 09:06:06 +0100

    Final Commit

---
+++ Siemens_DemonstratorTaktzeitMonitor/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace TaktzeitMonitor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.t_url = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(190, 12);
+            this.chart1.Name = "chart1";
+            this.chart1.Size = new System.Drawing.Size(1062, 962);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            this.chart1.Click += new System.EventHandler(this.chart1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(172, 63);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Vorgabe laden";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 81);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(172, 63);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Aufnahme";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // t_url
+            // 
+            this.t_url.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.t_url.Location = new System.Drawing.Point(12, 163);
+            this.t_url.Name = "t_url";
+            this.t_url.Size = new System.Drawing.Size(171, 20);
+            this.t_url.TabIndex = 4;
+            this.t_url.Text = "http://192.168.214.241:8080/";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1264, 986);
+            this.Controls.Add(this.t_url);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox t_url;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Siemens_DemonstratorTaktzeitMonitor/Form1.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+using System.Net;
+
+namespace TaktzeitMonitor
+{
+    public partial class Form1 : Form
+    {
+
+        double LastWertX = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void chart1_Click(object sender, EventArgs e)
+        {
+
+        }
+        //Vorgabe laden
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Title = "Konfigurationsdatei auswählen";
+            dialog.Filter = "CSV-Files |*.csv" + "|All Files|*.*";
+            if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                chart1.Series.Add("Vorgabe");
+                // Set SplineArea chart type
+                chart1.Series["Vorgabe"].ChartType = SeriesChartType.Area;
+                //Enable data points markers
+                //chart1.Series["Vorgabe"].MarkerStyle = MarkerStyle.Circle;
+                //chart1.Series["Vorgabe"].MarkerColor = Color.Magenta;
+                // Show transparent color
+                chart1.Series["Vorgabe"].Color = Color.FromArgb(60, 255, 0, 000);
+                try
+                {
+                    System.IO.StreamReader SRConf = new System.IO.StreamReader(dialog.FileName);
+
+                    while (SRConf.EndOfStream == false)
+                    {
+                        String Zeilenstream = SRConf.ReadLine();
+                        String[] Zeilenarray = Zeilenstream.Split(';');
+                        double WertX = Convert.ToDouble(Zeilenarray[0]);
+                        double WertY = Convert.ToDouble(Zeilenarray[1]);
+                        chart1.Series["Vorgabe"].Points.AddXY(WertX, WertY);
+                    }
+                    SRConf.Close();
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Eine Konfiguration wurde nicht gefunden. \r Die Konfigurationsdatei wird neu erstellt!");
+                }
+            }
+      
+        }
+
+        //Aufnahme starten
+        private void button2_Click(object sender, EventArgs e)
+        {
+            LastWertX = 0;
+            chart1.Series["Aktuell"].Points.Clear();
+            timer1.Interval = 1000;
+            timer1.Start();
+        }
+
+        private void Abfrage()
+        {
+            //try
+            //{
+                String Requeststring = t_url.Text + "?";
+                Requeststring = Requeststring + "&request=" + "/channel/channelDiagnose/cycleTime";
+                Requeststring = Requeststring + "&request=" + "/Channel/ProgramInfo/actLineNumber";
+                String Antwortstring = HTTPAbfrage(Requeststring);
+                Antwortstring = Antwortstring.Replace('.', ',');
+                String[] Antwortarray = Antwortstring.Split(';');
+                double WertX = Convert.ToDouble(Antwortarray[0]);
+                double WertY = Convert.ToDouble(Antwortarray[1]);
+                Console.WriteLine(WertX);
+                if (WertX > LastWertX)
+                {
+                    chart1.Series["Aktuell"].Points.AddXY(WertX, WertY);
+                    LastWertX = WertX;
+                }
+
+                
+
+
+           //}
+            //catch (Exception e)
+            //{
+            //    timer1.Stop();
+            //    MessageBox.Show("Fehler bei der Abfrage!\nBitte stoppen, dann alle Kanäle kontrollieren und erneut auf Start klicken!\n", "Fehler bei der Abfrage");
+
+            //}
+
+        }
+
+        string HTTPAbfrage(string requeststring)
+        {
+            HttpWebRequest Anfrage = (HttpWebRequest)WebRequest.Create(requeststring);
+            HttpWebResponse Antwort = (HttpWebResponse)Anfrage.GetResponse();
+            System.IO.Stream Antwort_Stream = Antwort.GetResponseStream();
+            System.IO.StreamReader Antwort_Streamreader = new System.IO.StreamReader(Antwort_Stream);
+            String Ausgabe = Antwort_Streamreader.ReadToEnd();
+            return Ausgabe;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Abfrage();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            chart1.Series.Clear();
+            //Anpassen der Achsen
+            chart1.ChartAreas[0].Axes[0].ArrowStyle = AxisArrowStyle.Triangle;
+            chart1.ChartAreas[0].Axes[1].ArrowStyle = AxisArrowStyle.Triangle;
+            chart1.ChartAreas[0].Axes[0].Title = "Job Time (D)";
+            chart1.ChartAreas[0].Axes[1].Title = "NC-Line (-)";
+            chart1.ChartAreas[0].Axes[0].Minimum = 0;
+            chart1.ChartAreas[0].Axes[1].Minimum = 0;
+            // Disable X axis margin
+            chart1.ChartAreas[0].AxisX.IsMarginVisible = true;
+            //Zoomfunktion Aktivieren
+            chart1.ChartAreas[0].AxisX.ScrollBar.Enabled = true;
+            chart1.ChartAreas[0].AxisX.ScrollBar.IsPositionedInside = true;
+            chart1.ChartAreas[0].AxisX.ScrollBar.ButtonStyle = ScrollBarButtonStyles.ResetZoom;
+            chart1.ChartAreas[0].AxisX.ScrollBar.Size = 20;
+            chart1.ChartAreas[0].CursorX.IsUserEnabled = true;
+            chart1.ChartAreas[0].CursorX.IsUserSelectionEnabled = true;
+            chart1.ChartAreas[0].AxisY.ScrollBar.Enabled = true;
+            chart1.ChartAreas[0].AxisY.ScrollBar.IsPositionedInside = true;
+            chart1.ChartAreas[0].AxisY.ScrollBar.ButtonStyle = ScrollBarButtonStyles.ResetZoom;
+            chart1.ChartAreas[0].AxisY.ScrollBar.Size = 20;
+            chart1.ChartAreas[0].CursorY.IsUserEnabled = true;
+            chart1.ChartAreas[0].CursorY.IsUserSelectionEnabled = true;
+
+            chart1.Series.Add("Aktuell");
+            chart1.Series["Aktuell"].ChartType = SeriesChartType.Line;
+            chart1.Series["Aktuell"].MarkerStyle = MarkerStyle.Circle;
+            chart1.Series["Aktuell"].MarkerColor = Color.Black;
+           
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+        }
+    }
+}
